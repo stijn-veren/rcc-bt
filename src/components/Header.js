4641d549db8aac85e28423013d702ca9e0db82bd
Stijn Veren <stijn.veren@gmail.com>
2021-10-24 02:01:25 +0300

    Add direct css styles in javascript

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 const Header = ({ title }) => {
   return (
     <header>
-      <h1>{title}</h1>
+      <h1 style={{ color: 'red', backgroundColor: 'black' }}>{title}</h1>
     </header>
   )
 }
