1c4114cd70f29a25c1fc224615e51a6ff71eb1cc
Stijn Veren <stijn.veren@gmail.com>
2021-10-24 02:14:28 +0300

    Add onclick event

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,16 @@
 import PropTypes from 'prop-types'
 
 const Button = ({ color, text }) => {
+  const onClick = () => {
+    console.log('click')
+  }
+  
   return (
-    <button style={{ backgroundColor: color }} className="btn">
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
       {text}
     </button>
   )
