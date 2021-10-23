5177c291df731f668200f5ad295af3787a479201
Stijn Veren <stijn.veren@gmail.com>
2021-10-24 02:04:38 +0300

    Remove extra css rules

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,9 @@
 import PropTypes from 'prop-types'
 
-const headingStyle = {
-  color: 'red',
-  backgroundColor: 'black',
-}
-
 const Header = ({ title }) => {
   return (
     <header>
-      <h1 style={headingStyle}>{title}</h1>
+      <h1>{title}</h1>
     </header>
   )
 }
