32e9a0613042a5d0c51d3128f17fdf37f6e92f97
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-24 19:49:27 +0300

    Add delete button mechanism via props

--- src/App.js
+++ src/App.js
@@ -24,10 +24,14 @@ function App() {
     },
   ])
 
+  const deleteTask = id => {
+    console.log('delete', id)
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   )
 }
