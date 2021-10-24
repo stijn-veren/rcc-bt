70f0dc85ea4428fe9651d2a812c4d6490cc42adb
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-24 20:23:47 +0300

    Add task reminder toggle props and state update

--- src/App.js
+++ src/App.js
@@ -24,16 +24,26 @@ function App() {
     },
   ])
 
-  const deleteTask = id => {
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
     setTasks(
-      tasks.filter(task => task.id !== id)
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
     )
   }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'No Tasks To Show'
+      )}
     </div>
   )
 }
